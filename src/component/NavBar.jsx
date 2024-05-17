bd0c671e974342cb1ce74a3a7d5c9b1f469ca3e0
unknown <ckushal106@gmail.com>
2024-05-17 19:46:28 +0530

    commit

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, styled, Box } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, styled, Box, Menu, MenuItem } from '@mui/material';
 import { NavLink } from 'react-router-dom';
-import logo from './image.png'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import logo from './image.png'; // Adjust the path as needed
+
 const Header = styled(AppBar)`
   background: #111111;
 `;
 
 const Tab = styled(NavLink)`
   font-size: 20px;
-  margin-right: 30px;
+  margin-right: 20px;
   color: inherit;
-  text-decoration: ;
+  text-decoration: none;
 
   @media (max-width: 600px) {
     font-size: 16px;
@@ -19,18 +21,44 @@ const Tab = styled(NavLink)`
 `;
 
 const NavBar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Header position="static">
       <Toolbar>
         <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
-          <img src={logo} alt="logo" style={{ height: '40px', marginRight: '15px' }} />
+          <img src={logo} alt="logo" style={{ height: '40px', marginRight: '10px' }} />
           <Typography variant="h6">
-            
+            Ecommerce
           </Typography>
         </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end', flexGrow: 1 }}>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', flexGrow: 1 }}>
           <Tab to="/">Products</Tab>
-          <Tab to="add">Resources</Tab>
+          <Box sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer', marginRight: '20px', color: 'inherit' }} onClick={handleClick}>
+            <Typography sx={{ fontSize: '20px', textDecoration: 'none' }}>
+              Resources
+            </Typography>
+            <ArrowDropDownIcon />
+          </Box>
+          <Menu
+            id="resources-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose} component={NavLink} to="/resource1">Resource 1</MenuItem>
+            <MenuItem onClick={handleClose} component={NavLink} to="/resource2">Resource 2</MenuItem>
+            <MenuItem onClick={handleClose} component={NavLink} to="/resource3">Resource 3</MenuItem>
+          </Menu>
           <Tab to="view">Support</Tab>
         </Box>
       </Toolbar>
