840376860bd9fb1295e7d095b39a53b64ba2633f
unknown <ckushal106@gmail.com>
2024-05-17 19:00:30 +0530

    commit

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,28 +1,41 @@
-import {AppBar,Toolbar,Typography, styled}from '@mui/material';
+import React from 'react';
+import { AppBar, Toolbar, Typography, styled, Box } from '@mui/material';
 import { NavLink } from 'react-router-dom';
-const Header=styled(AppBar)
-`
-background:#111111
-`
-const Tab=styled(NavLink)`
-font-size:20px;
-margin-right:20px;
-color:inherit;
-text-decoration:none; 
-`
+import logo from './image.png'
+const Header = styled(AppBar)`
+  background: #111111;
+`;
 
-const NavBar=()=>{
-    return(
-       <Header position='static'>
-        
-        <Toolbar>
-        <Tab>Ecommerce</Tab>
-        <Tab to='/'>Home</Tab>
-        <Tab to='add'>AddUser</Tab>
-        <Tab to='view'>ViewUser</Tab>
+const Tab = styled(NavLink)`
+  font-size: 20px;
+  margin-right: 30px;
+  color: inherit;
+  text-decoration: ;
 
-        </Toolbar>
-       </Header>
-    )
-}
+  @media (max-width: 600px) {
+    font-size: 16px;
+    margin-right: 10px;
+  }
+`;
+
+const NavBar = () => {
+  return (
+    <Header position="static">
+      <Toolbar>
+        <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
+          <img src={logo} alt="logo" style={{ height: '40px', marginRight: '15px' }} />
+          <Typography variant="h6">
+            
+          </Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', flexGrow: 1 }}>
+          <Tab to="/">Products</Tab>
+          <Tab to="add">Resources</Tab>
+          <Tab to="view">Support</Tab>
+        </Box>
+      </Toolbar>
+    </Header>
+  );
+};
+
 export default NavBar;
